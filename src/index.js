5bcc1c16367b645e5ebb90ae0f69e1ca705f6761
Gonzalo Marco <gonzalo.marco@xmartlabs.com>
2022-11-25 14:52:18 -0300

    function components

--- src/index.js
+++ src/index.js
@@ -15,6 +15,17 @@ class Square extends React.Component {
   }
 }
 
+function Square(props){
+  return(
+    <button
+      className="square"
+      onClick={() => props.onClick()}
+      >
+        {props.value}
+    </button>
+  )
+}
+
 class Board extends React.Component {
   constructor(props) {
     super(props);
