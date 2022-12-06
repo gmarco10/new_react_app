eb53236823a463cabef416c2f7603770db3b869d
gmarco10 <gmarco2021@gmail.com>
2022-12-06 16:17:08 -0300

    dividing component into different files

---
+++ src/Board.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import SquareExtern  from './Square';
+
+// import './index.css';
+
+const Board = (props) => {
+  // Check that, in react inspector, Square component is named as Square instead of SquareExtern
+  const renderSquare = (i) => {
+    return <SquareExtern
+            value={props.squares[i]}
+            onClick={() => props.onClick(i)}
+          />
+  }
+
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+}
+
+export { Board };
